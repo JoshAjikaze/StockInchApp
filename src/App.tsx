dc3cb13434f49e492e117fab2360d046e0277a4f
ajiwojuolorunjoshua <ajiwojuolorunjoshua@gmail.com>
2024-06-09 23:20:56 +0000

    Partial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 
 const App = () => {
   return (
-    <div className="w-full h-screen text-white bg-black">App</div>
+    <div className="w-full h-screen text-white bg-black lg:hidden">App</div>
   )
 }
 
